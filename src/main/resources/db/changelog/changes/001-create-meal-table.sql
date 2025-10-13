3f0cd39e8deee82d93160b7a498f4f952b422e7b
tesinitsyn <sitiev174@gmail.com>
2025-10-13 17:33:16 +0300

    fix for ci to work

--- src/main/resources/db/changelog/changes/001-create-meal-table.sql
+++ src/main/resources/db/changelog/changes/001-create-meal-table.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset timofey:001
-CREATE TABLE meal (
+CREATE TABLE IF NOT EXISTS meal  (
                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                       user_id UUID NOT NULL,
                       name VARCHAR(255) NOT NULL,
