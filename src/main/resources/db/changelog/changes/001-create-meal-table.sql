1553a42bcdfa9d968772d6dae453f25394d44821
tesinitsyn <sitiev174@gmail.com>
2025-10-21 14:02:45 +0300

    fix migrations

--- src/main/resources/db/changelog/changes/001-create-meal-table.sql
+++ src/main/resources/db/changelog/changes/001-create-meal-table.sql
@@ -1,18 +1,18 @@
 --liquibase formatted sql
---changeset tesinitsyn:001
 
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE IF NOT EXISTS meal (
-                                    id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                                    user_id      UUID        NOT NULL,
-                                    name         VARCHAR(255) NOT NULL CHECK (char_length(name) > 0),
-                                    description  TEXT,
-                                    calories     INTEGER     CHECK (calories >= 0),
-                                    created_at   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
+--changeset tesinitsyn:002
+CREATE TABLE IF NOT EXISTS meal
+(
+    id             UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
+    user_id        UUID         NOT NULL,
+    name           VARCHAR(255) NOT NULL CHECK (char_length(name) > 0),
+    description    TEXT,
+    total_calories INTEGER CHECK (total_calories >= 0),
+    meal_time      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    source         VARCHAR(20)           DEFAULT 'manual' CHECK (source IN ('manual', 'photo', 'ai')),
+    created_at     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX IF NOT EXISTS idx_meal_user_id ON meal (user_id);
-CREATE INDEX IF NOT EXISTS idx_meal_created_at ON meal (created_at DESC);
-
---rollback DROP TABLE IF EXISTS meal;
+CREATE INDEX IF NOT EXISTS idx_meal_meal_time ON meal (meal_time);
+--roll
