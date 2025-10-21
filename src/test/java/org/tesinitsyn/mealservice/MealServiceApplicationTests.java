67d67a2d14075e4a8b82d214058701f29d542a31
tesinitsyn <sitiev174@gmail.com>
2025-10-21 14:21:55 +0300

    fix ollama context test

--- src/test/java/org/tesinitsyn/mealservice/MealServiceApplicationTests.java
+++ src/test/java/org/tesinitsyn/mealservice/MealServiceApplicationTests.java
@@ -2,10 +2,13 @@ package org.tesinitsyn.mealservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.tesinitsyn.mealservice.ai.service.ChatMemoryService;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@MockBean(ChatMemoryService.class) // пример: замокаем бин, который ходит в Ollama
 class MealServiceApplicationTests {
 
     @Test
